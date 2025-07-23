a3a793bcc20fef7443400e0a07de83d47a99ae75
Damon Tingey <damon.tingey@gmail.com>
2025-07-23 16:27:45 -0600

    lsp and lint

--- init.lua
+++ init.lua
@@ -3,79 +3,79 @@ vim.cmd.colorscheme("default")
 vim.g.have_nerd_font = true
 
 -- basic settings
-vim.o.number = true -- line numbers
+vim.o.number = true         -- line numbers
 vim.o.relativenumber = true -- relative line numbers
-vim.o.cursorline = true -- highlight current line
-vim.o.wrap = false -- don't wrap lines
-vim.o.scrolloff = 10 -- keep 10 lines above/below cursor
-vim.o.sidescrolloff = 8 -- keep 8 lines above/below cursor
+vim.o.cursorline = true     -- highlight current line
+vim.o.wrap = false          -- don't wrap lines
+vim.o.scrolloff = 10        -- keep 10 lines above/below cursor
+vim.o.sidescrolloff = 8     -- keep 8 lines above/below cursor
 
 -- Indentation
-vim.o.tabstop = 2 -- tab width
-vim.o.shiftwidth = 2 -- indent width
-vim.o.softtabstop = 2 -- soft tab stop
-vim.o.expandtab = true -- use spaces instead of tabs
+vim.o.tabstop = 2        -- tab width
+vim.o.shiftwidth = 2     -- indent width
+vim.o.softtabstop = 2    -- soft tab stop
+vim.o.expandtab = true   -- use spaces instead of tabs
 vim.o.smartindent = true -- smart auto-indenting
-vim.o.autoindent = true -- copy indent from current line
+vim.o.autoindent = true  -- copy indent from current line
 
 -- Search settings
 vim.o.ignorecase = true -- case insensitive search
-vim.o.smartcase = true -- Case sensitive if uppercase in search
-vim.o.hlsearch = false -- Don't highlight search results
-vim.o.incsearch = true -- Show matches as you type
+vim.o.smartcase = true  -- Case sensitive if uppercase in search
+vim.o.hlsearch = false  -- Don't highlight search results
+vim.o.incsearch = true  -- Show matches as you type
 
 -- Visual Settings
-vim.o.termguicolors = true -- Enable 24 bit color
-vim.o.signcolumn = "yes" -- Always show sign column
-vim.o.colorcolumn = "120" -- Show column at 120 chars
-vim.o.showmatch = true -- Highlight matching brackets
-vim.o.matchtime = 2 -- How long to show matching brackets
+vim.o.termguicolors = true                      -- Enable 24 bit color
+vim.o.signcolumn = "yes"                        -- Always show sign column
+vim.o.colorcolumn = "120"                       -- Show column at 120 chars
+vim.o.showmatch = true                          -- Highlight matching brackets
+vim.o.matchtime = 2                             -- How long to show matching brackets
 vim.o.cmdheight = 1
 vim.o.completeopt = "menuone,noinsert,noselect" -- completion options
-vim.o.showmode = false -- Don't show mode in command line
-vim.o.pumheight = 10 -- Popup menu height
-vim.o.pumblend = 10 -- popup menu transparency
-vim.o.winblend = 0 -- floating window transparency
-vim.o.conceallevel = 0 -- don't hide markup
-vim.o.concealcursor = "" -- don't hide cursor line markup
-vim.o.lazyredraw = true -- don't redraw during macros
-vim.o.synmaxcol = 300 -- Syntax highlighting limit
+vim.o.showmode = false                          -- Don't show mode in command line
+vim.o.pumheight = 10                            -- Popup menu height
+vim.o.pumblend = 10                             -- popup menu transparency
+vim.o.winblend = 0                              -- floating window transparency
+vim.o.conceallevel = 0                          -- don't hide markup
+vim.o.concealcursor = ""                        -- don't hide cursor line markup
+vim.o.lazyredraw = true                         -- don't redraw during macros
+vim.o.synmaxcol = 300                           -- Syntax highlighting limit
 
 -- File handling
-vim.o.backup = false -- Don't create backup files
+vim.o.backup = false      -- Don't create backup files
 vim.o.writebackup = false -- Don't create backup before writing
-vim.o.swapfile = false -- Don't create swap files
-vim.o.undofile = true -- persistent undo
-vim.o.updatetime = 250 -- faster completion
-vim.o.timeoutlen = 500 -- Key timeout duration
-vim.o.ttimeoutlen = 0 -- Key code timeout
-vim.o.autoread = true -- Auto reload files changed outside of vim
-vim.o.autowrite = false -- Don't autosave
+vim.o.swapfile = false    -- Don't create swap files
+vim.o.undofile = true     -- persistent undo
+vim.o.updatetime = 250    -- faster completion
+vim.o.timeoutlen = 500    -- Key timeout duration
+vim.o.ttimeoutlen = 0     -- Key code timeout
+vim.o.autoread = true     -- Auto reload files changed outside of vim
+vim.o.autowrite = false   -- Don't autosave
 
 -- Behavior settings
-vim.o.hidden = true -- Allow hidden buffers
-vim.o.errorbells = false -- No error bells
+vim.o.hidden = true                  -- Allow hidden buffers
+vim.o.errorbells = false             -- No error bells
 vim.o.backspace = "indent,eol,start" -- better backspace behavior
-vim.o.autochdir = false -- don't auto change directory
-vim.opt.path:append("**") -- include subdirectories in search
-vim.o.mouse = "a" -- enable mouse mode
+vim.o.autochdir = false              -- don't auto change directory
+vim.opt.path:append("**")            -- include subdirectories in search
+vim.o.mouse = "a"                    -- enable mouse mode
 vim.schedule(function()
-	vim.o.clipboard = "unnamedplus"
-end) -- schedule using system clipboard
+  vim.o.clipboard = "unnamedplus"
+end)                    -- schedule using system clipboard
 vim.o.modifiable = true -- allow buffer modifications
 vim.o.encoding = "UTF-8"
 
 -- Folding settings
-vim.o.foldmethod = "expr" -- Use expression for folding
+vim.o.foldmethod = "expr"                     -- Use expression for folding
 vim.o.foldexpr = "nvim_treesitter#foldexpr()" -- use treesitter for folding
-vim.o.foldlevel = 99 -- start with all folds open
+vim.o.foldlevel = 99                          -- start with all folds open
 
 -- Split behavior
 vim.o.splitbelow = true -- Horizontal splits go right
 vim.o.splitright = true -- Vertical splits go right
 
 -- Key mappings
-vim.g.mapleader = " " -- Set leader key to space
+vim.g.mapleader = " "      -- Set leader key to space
 vim.g.maplocalleader = " " -- Set local leader key to space
 
 -- Normal Mode Mappings
@@ -149,9 +149,9 @@ vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" }
 
 -- Copy Full File-Path
 vim.keymap.set("n", "<leader>pa", function()
-	local path = vim.fn.expand("%:p")
-	vim.fn.setreg("+", path)
-	print("file:", path)
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  print("file:", path)
 end)
 
 -- Autocommands
@@ -162,52 +162,52 @@ local augroup = vim.api.nvim_create_augroup("UserConfig", {})
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = augroup,
-	callback = function()
-		vim.hl.on_yank()
-	end,
+  desc = "Highlight when yanking (copying) text",
+  group = augroup,
+  callback = function()
+    vim.hl.on_yank()
+  end,
 })
 
 -- Return to last edit position when opening files
 vim.api.nvim_create_autocmd("BufReadPost", {
-	group = augroup,
-	callback = function()
-		local mark = vim.api.nvim_buf_get_mark(0, '"')
-		local lcount = vim.api.nvim_buf_line_count(0)
-		if mark[1] > 0 and mark[1] <= lcount then
-			pcall(vim.api.nvim_win_set_cursor, 0, mark)
-		end
-	end,
+  group = augroup,
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
 })
 
 -- Auto-close terminal when process exits
 vim.api.nvim_create_autocmd("TermClose", {
-	group = augroup,
-	callback = function()
-		if vim.v.event.status == 0 then
-			vim.api.nvim_buf_delete(0, {})
-		end
-	end,
+  group = augroup,
+  callback = function()
+    if vim.v.event.status == 0 then
+      vim.api.nvim_buf_delete(0, {})
+    end
+  end,
 })
 
 -- Auto-resize splits when window is resized
 vim.api.nvim_create_autocmd("VimResized", {
-	group = augroup,
-	callback = function()
-		vim.cmd("tabdo wincmd =")
-	end,
+  group = augroup,
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
 })
 
 -- Create directories when saving files
 vim.api.nvim_create_autocmd("BufWritePre", {
-	group = augroup,
-	callback = function()
-		local dir = vim.fn.expand("<afile>:p:h")
-		if vim.fn.isdirectory(dir) == 0 then
-			vim.fn.mkdir(dir, "p")
-		end
-	end,
+  group = augroup,
+  callback = function()
+    local dir = vim.fn.expand("<afile>:p:h")
+    if vim.fn.isdirectory(dir) == 0 then
+      vim.fn.mkdir(dir, "p")
+    end
+  end,
 })
 
 -- ============================================================================
@@ -218,11 +218,11 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		error("Error cloning lazy.nvim:\n" .. out)
-	end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    error("Error cloning lazy.nvim:\n" .. out)
+  end
 end
 
 ---@type vim.Option
@@ -230,91 +230,92 @@ local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
 plugins_i_want = {
-	"nvim-lint",
-	"tokyonight",
-	"whichkey",
-	"gitsigns",
-	"nvim-tree",
-	"copilot.vim",
-	"telescope",
-	"blink.cmp",
-	"conform.nvim",
+  "nvim-lint",
+  "tokyonight",
+  "whichkey",
+  "gitsigns",
+  "nvim-tree",
+  "copilot.vim",
+  "telescope",
+  "blink.cmp",
+  "conform.nvim",
 }
 
 require("lazy").setup({
 
-	{ -- Colorscheme
-		"shaunsingh/nord.nvim",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
-		config = function()
-			vim.g.nord_italic = false
-			vim.g.nord_borders = false
-			vim.g.nord_bold = false
+  {                -- Colorscheme
+    "shaunsingh/nord.nvim",
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      vim.g.nord_italic = false
+      vim.g.nord_borders = false
+      vim.g.nord_bold = false
 
-			require("nord").set()
-		end,
-	},
+      require("nord").set()
+    end,
+  },
 
-	"NMAC427/guess-indent.nvim", -- Detect tabstop and shiftwidth automatically
+  "NMAC427/guess-indent.nvim", -- Detect tabstop and shiftwidth automatically
 
-	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
-		},
-	},
+  {                           -- Adds git related signs to the gutter, as well as utilities for managing changes
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+    },
+  },
 
-	{ -- github copilot
-		"github/copilot.vim",
-		config = function()
-			vim.keymap.set("i", "<C-j>", 'copilot#Accept("\\<CR>")', {
-				expr = true,
-				replace_keycodes = false,
-			})
-			vim.g.copilot_no_tab_map = true
-		end,
-	},
-	require("plugins.which-key"), -- keybinds helper     TODO: remove this when comfortable
-	require("plugins.nvim-tree"), -- sidebar file explorer
-	require("plugins.telescope"), -- fuzzy finder
-	require("plugins.lsp"), -- LSP Configuration
-	require("plugins.conform"), -- Autoformatting
-	require("plugins.autocomplete"), -- Autocompletion
-	require("plugins.mini"),
-	require("plugins.treesitter"), -- Treesitter config
+  { -- github copilot
+    "github/copilot.vim",
+    config = function()
+      vim.keymap.set("i", "<C-j>", 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+      vim.g.copilot_no_tab_map = true
+    end,
+  },
+  require("plugins.which-key"),   -- keybinds helper     TODO: remove this when comfortable
+  require("plugins.nvim-tree"),   -- sidebar file explorer
+  require("plugins.telescope"),   -- fuzzy finder
+  require("plugins.lsp"),         -- LSP Configuration
+  require("plugins.conform"),     -- Autoformatting
+  require("plugins.autocomplete"), -- Autocompletion
+  require("plugins.mini"),
+  require("plugins.treesitter"),  -- Treesitter config
+  require("plugins.lint"),        -- Linting
 
-	-- require 'kickstart.plugins.debug',
-	-- require 'kickstart.plugins.indent_line',
-	-- require 'kickstart.plugins.lint',
-	-- require 'kickstart.plugins.autopairs',
-	-- require 'kickstart.plugins.neo-tree',
-	-- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-	require("custom.plugins.sqlite").setup(),
-	require("custom.plugins.terminal").setup(),
+  -- require 'kickstart.plugins.debug',
+  -- require 'kickstart.plugins.indent_line',
+  -- require 'kickstart.plugins.lint',
+  -- require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.neo-tree',
+  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require("custom.plugins.sqlite").setup(),
+  require("custom.plugins.terminal").setup(),
 }, {
-	ui = {
-		-- If you are using a Nerd Font: set icons to an empty table which will use the
-		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-		icons = vim.g.have_nerd_font and {} or {
-			cmd = "⌘",
-			config = "🛠",
-			event = "📅",
-			ft = "📂",
-			init = "⚙",
-			keys = "🗝",
-			plugin = "🔌",
-			runtime = "💻",
-			require = "🌙",
-			source = "📄",
-			start = "🚀",
-			task = "📌",
-			lazy = "💤 ",
-		},
-	},
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = "⌘",
+      config = "🛠",
+      event = "📅",
+      ft = "📂",
+      init = "⚙",
+      keys = "🗝",
+      plugin = "🔌",
+      runtime = "💻",
+      require = "🌙",
+      source = "📄",
+      start = "🚀",
+      task = "📌",
+      lazy = "💤 ",
+    },
+  },
 })
---
+++ lua/plugins/lint.lua
@@ -0,0 +1,85 @@
+return {
+
+	{ -- Linting
+		"mfussenegger/nvim-lint",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local lint = require("lint")
+			lint.linters_by_ft = {
+				-- markdown = { 'markdownlint' },
+				python = { "mypy", "ruff" },
+			}
+
+			local function find_venv_python()
+				local path = os.getenv("VIRTUAL_ENV") and os.getenv("VIRTUAL_ENV") .. "/bin/python"
+					or vim.fn.getcwd() .. "/.venv/bin/python"
+				return path
+			end
+
+			local mypy = lint.linters.mypy
+			local new_args = { "--python-executable", find_venv_python() }
+			for _, v in ipairs(new_args) do
+				table.insert(mypy.args, v)
+			end
+			-- mypy.args = { '--python-executable', find_venv_python(), '--show-column-numbers' }
+			-- To allow other plugins to add linters to require('lint').linters_by_ft,
+			-- instead set linters_by_ft like this:
+			-- lint.linters_by_ft = lint.linters_by_ft or {}
+			-- lint.linters_by_ft['markdown'] = { 'markdownlint' }
+			--
+			-- However, note that this will enable a set of default linters,
+			-- which will cause errors unless these tools are available:
+			-- {
+			--   clojure = { "clj-kondo" },
+			--   dockerfile = { "hadolint" },
+			--   inko = { "inko" },
+			--   janet = { "janet" },
+			--   json = { "jsonlint" },
+			--   markdown = { "vale" },
+			--   rst = { "vale" },
+			--   ruby = { "ruby" },
+			--   terraform = { "tflint" },
+			--   text = { "vale" }
+			-- }
+			--
+			-- You can disable the default linters by setting their filetypes to nil:
+			-- lint.linters_by_ft['clojure'] = nil
+			-- lint.linters_by_ft['dockerfile'] = nil
+			-- lint.linters_by_ft['inko'] = nil
+			-- lint.linters_by_ft['janet'] = nil
+			-- lint.linters_by_ft['json'] = nil
+			-- lint.linters_by_ft['markdown'] = nil
+			-- lint.linters_by_ft['rst'] = nil
+			-- lint.linters_by_ft['ruby'] = nil
+			-- lint.linters_by_ft['terraform'] = nil
+			-- lint.linters_by_ft['text'] = nil
+
+			-- Create autocommand which carries out the actual linting
+			-- on the specified events.
+			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+				group = lint_augroup,
+				callback = function()
+					-- Only run the linter in buffers that you can modify in order to
+					-- avoid superfluous noise, notably within the handy LSP pop-ups that
+					-- describe the hovered symbol using Markdown.
+					if vim.bo.modifiable then
+						lint.try_lint()
+					end
+				end,
+			})
+
+			local lint_progress = function()
+				local linters = lint.get_running()
+				if #linters == 0 then
+					return " "
+				end
+				return " " .. table.concat(linters, ", ")
+			end
+
+			vim.api.nvim_create_user_command("LintProgress", function()
+				print(lint_progress())
+			end, {})
+		end,
+	},
+}
